25bbce191a569efcaf70752f154e7a0d85398a79
Ezra <56590002+EzraIndigo@users.noreply.github.com>
2019-10-18 16:25:56 +0100

    Update mapDraw.js

--- website directory/js/mapDraw.js
+++ website directory/js/mapDraw.js
@@ -1,14 +1,38 @@
 $( document ).ready(function() {
+    
     console.log( "ready!" );
-
+    var currentMap = "floorplans/Ground_Floor.svg";
     var m = Snap('#svg');
-    var group = m.group();
-    Snap.load("floorplans/Ground_Floor.svg", onSVGLoaded);
+    var MapGroup = m.group();
+    Snap.load(currentMap, onSVGLoaded);
+    var mapArray = [];
 
     function onSVGLoaded (svg) {
-        group.append( svg );
-        group.hover(function() { console.log("test");});
-        }
+        //  m.append(layer0);
+        MapGroup.append(svg);
+        getMapLayers();
+
+    }
+
+    function getMapLayers() {
+        //traverse through the 'g' layers in the svg tree and pick out the
+        //inkscape layer names (not just 'layer1' but the inscape:label ID)
+        var local = MapGroup.selectAll('g');
+        local.forEach(function(el){
+            var p = el.select("inkscape:label");
+            mapArray[el.attr('g')]; //this is coming up empty
+        });
+        console.log(mapArray);
+        console.log(local);
+
+    }
+
+    function getLayerChildObjects(layerID) {
+
+    }
+        
+
+    
 
 
 
