6414c12200e077dc539c957d2239bb28eb057a5f
Ezra <56590002+EzraIndigo@users.noreply.github.com>
2019-11-14 17:45:41 +0000

    Core JS Issues solved
    Indexing of svg object(rect) IDs is now possible - stores in array
    
    Obtaining current ID of svg graphic clicked is now possible paving way for PHP call to future DB to return Room data based on ID returned from click

--- website directory/js/mapLoadv2.js
+++ website directory/js/mapLoadv2.js
@@ -7,7 +7,32 @@ $(window).on('load', (function(){
     var a = document.getElementById('stage');
     var svgDoc = a.contentDocument;
     var svgItem = svgDoc.getElementById("Offices");
+    
+    var b = svgItem.getElementsByTagName("rect");
+    console.log(svgDoc);
     console.log(svgItem);
+    console.log(b);
+    var rectID = new Array();
 
+ 
+    
+    
+    for(i=0; i < b.length; i++) {
+        rectID.push(b[i].id);
+    }
+    
+    
+
+    svgItem.onclick = function() {
+        
+        var currentID = event.target.id;
+    }
+    
+    function getRoomData(currentID) {
+        //pass id of current room to this func
+        //php script queries DB with roomID providing roomID is PK
+        //php returns dataset, loops through and displays
+        //as side popup
+    }
     
 }));
