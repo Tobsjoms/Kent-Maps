32151b3259978d59bac0056149f5a4e52af3e1bd
Ezra <56590002+EzraIndigo@users.noreply.github.com>
2020-03-10 21:00:36 +0000

    corrected DB credentials

--- Website/inc/php/database.php
+++ Website/inc/php/database.php
@@ -1,8 +1,8 @@
 <?php
 //database credentials
-	$servername = "krelly.com.mysql";
-	$username = "krelly_com";
-	$password = "DKdKX4mtPLCDUhv6Tf237xjc";
+	$server = "krelly.com.mysql";
+	$dbusername = "krelly_com";
+	$dbpassword = "DKdKX4mtPLCDUhv6Tf237xjc";
 	$dbname = "krelly_com";
 
 
